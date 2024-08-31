df22b6259c48788d797cdf713c33c5c7157dd5ea
laviniafarani <laviniafarani@gmail.com>
2024-08-31 18:59:16 -0300

    chore: update api folder __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,8 @@
 # from utils import spotify_connection
 from .artist_data import get_user_top_artists
-from .track_data import get_user_top_tracks, get_tracks_audio_features
+from .track_data import get_user_top_tracks
 
 __all__ = [
     "get_user_top_artists",
-    "get_user_top_tracks",
-    "get_tracks_audio_features"
+    "get_user_top_tracks"
 ]
